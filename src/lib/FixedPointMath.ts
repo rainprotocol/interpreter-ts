200de5e2ead07cf1725db0f92b2e912590dc39ff
rouzwelt <rouzweltt@gmail.com>
2022-11-25 21:02:47 +0000

    Update FixedPointMath.ts

--- src/lib/FixedPointMath.ts
+++ src/lib/FixedPointMath.ts
@@ -6,8 +6,8 @@ import { eighteenZeros } from "../utils";
  */
 export const scale18 = (value: BigNumber, scale: number) : BigNumber => {
     return scale <= 18
-        ? value.mul((10 ** (18 - scale)).toString())
-        : value.div((10 ** (scale - 18)).toString())
+        ? value.mul('1' .padEnd(18 - scale, '0'))
+        : value.div('1' .padEnd(scale - 18, '0'))
 }
 
 /**
@@ -16,10 +16,10 @@ export const scale18 = (value: BigNumber, scale: number) : BigNumber => {
 export const scaleBy = (value: BigNumber, scale: number) : BigNumber => {
     if (scale > 127) {
         scale = 256 - scale
-        return value.div((10 ** (scale)).toString())
+        return value.div('1' .padEnd(scale, '0'))
     }
     else {
-        return value.mul((10 ** (scale)).toString())
+        return value.mul('1' .padEnd(scale, '0'))
     }
 }
 
@@ -28,8 +28,8 @@ export const scaleBy = (value: BigNumber, scale: number) : BigNumber => {
  */
 export const scaleN = (value: BigNumber, scale: number) : BigNumber => {
     return scale <= 18
-        ? value.div((10 ** (18 - scale)).toString())
-        : value.mul((10 ** (scale - 18)).toString())
+        ? value.div('1' .padEnd(18 - scale, '0'))
+        : value.mul('1' .padEnd(scale - 18, '0'))
 }
 
 /**
@@ -38,8 +38,8 @@ export const scaleN = (value: BigNumber, scale: number) : BigNumber => {
 export const fixedPointDiv = (value1: BigNumber, value2: BigNumber, scale: number) : BigNumber => {
     return (
         scale <= 18
-            ? value1.mul((10 ** (18 - scale)).toString())
-            : value1.div((10 ** (scale - 18)).toString())
+            ? value1.mul('1' .padEnd(18 - scale, '0'))
+            : value1.div('1' .padEnd(scale - 18, '0'))
     )
         .mul(eighteenZeros).div(value2)
 }
@@ -50,8 +50,8 @@ export const fixedPointDiv = (value1: BigNumber, value2: BigNumber, scale: numbe
 export const fixedPointMul = (value1: BigNumber, value2: BigNumber, scale: number) : BigNumber => {
     return value2.mul(
         scale <= 18
-            ? value1.mul((10 ** (18 - scale)).toString())
-            : value1.div((10 ** (scale - 18)).toString())
+            ? value1.mul('1' .padEnd(18 - scale, '0'))
+            : value1.div('1' .padEnd(scale - 18, '0'))
     )
         .div(eighteenZeros)
 }
